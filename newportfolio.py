99bdbce6fbc2f688b7651a25f293fa61fd5d7cba
fschnefeld <49908151+fschnefeld@users.noreply.github.com>
2024-06-27 15:42:40 +0200

    With pygwalker example

--- newportfolio.py
+++ newportfolio.py
@@ -7,6 +7,8 @@ from nbconvert import HTMLExporter
 from IPython.display import HTML
 #from streamlit_embedcode import github_gist
 import base64
+import bokeh
+import pygwalker as pyg
 
 # Set the URLs for embedding
 looker_url = "https://lookerstudio.google.com/embed/reporting/bf900ecb-3657-4901-b5bd-ab8899411118/page/p_e27a3gsx4c"
@@ -275,7 +277,20 @@ if selected_page == "Data Analysis":
     ## Welcome to the Data Analysis page
     This page allows you to interact with a sample analysis I have created using a demo dataset.
     """)
-    
+
+    # Load Titanic dataset
+    url = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+    df = pd.read_csv(url)
+
+    # Create PyGWalker visualization
+    gwalker = pyg.walk(df)
+
+    # Streamlit app
+    st.title("Interactive DataFrame Exploration with PyGWalker")
+    st.write("This app demonstrates how to create an interactive UI for a pandas DataFrame using PyGWalker.")
+
+    # Render PyGWalker visualization
+    st.markdown(gwalker.to_html(), unsafe_allow_html=True)
     
 if selected_page == "Data Pipelines":
     st.header("Data Pipelines")
